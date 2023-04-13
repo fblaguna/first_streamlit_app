8140977b531a0886fff83d7efde4963deaa6a4c9
fblaguna <130606012+fblaguna@users.noreply.github.com>
2023-04-13 12:45:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,32 +24,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#create the repreatable code block called a function
-def get_fruityvice_data(fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-
-#new section to display fruityvice api response 
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-      streamlit.error("Please select a fruit to get information")
-  else:
-      back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)
-  
-except URLError as e:
-     streamlit.error()
-
-streamlit.header("The fruit load list contains:")
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit list now contains:")
-streamlit.dataframe(my_own_rows)
 
 #allow user to add fruit to list
 def insert_row_snowflake(new_fruit):
