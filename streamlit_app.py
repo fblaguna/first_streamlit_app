0576824c729b81853d66511cebfc46a7ee116587
fblaguna <130606012+fblaguna@users.noreply.github.com>
2023-04-13 12:53:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,11 +25,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-#allow user to add fruit to list
-def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
-        return "Thanks for adding " + new_fruit
+#New section to display fruityvice api response 
+streamlit.header('Fruityvice Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+        else:
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+            streamlit.dataframe(fruityvice_normalized)
+            
+            except URLError as e:
+                streamlit.error()
 
 #add a button to load fruit 
 if streamlit.button('Get Fruit List'):
@@ -37,3 +45,10 @@ if streamlit.button('Get Fruit List'):
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
     streamlit.dataframe(my_data_rows)
+
+    
+    #allow user to add fruit to list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('" + new_fruit +"')")
+        return "Thanks for adding " + new_fruit
